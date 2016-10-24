7c7234e8fb6ac63fdb1fe766df142c646a72a810
rokhlin <rohlinav@gmail.com>
2016-10-24 18:37:48 +0300

    first commit

--- app/src/main/java/com/selfapps/rav/alltogether/BaseActivity.java
+++ app/src/main/java/com/selfapps/rav/alltogether/BaseActivity.java
@@ -3,9 +3,15 @@ package com.selfapps.rav.alltogether;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.selfapps.rav.alltogether.fragments.GroupsFragment;
+
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -16,7 +22,13 @@ public class BaseActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Fragment groupsFrag = new GroupsFragment();
+        FragmentTransaction fTrans = getSupportFragmentManager().beginTransaction();
+        fTrans.add(R.id.frameLayoutBaseActivity, groupsFrag);
+        fTrans.commit();
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabBaseActivity);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
