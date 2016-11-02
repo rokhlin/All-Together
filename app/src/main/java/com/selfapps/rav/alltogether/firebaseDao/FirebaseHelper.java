6c7f4fdb8a26f939e3cd7601dc6e107b734994d5
rokhlin <rohlinav@gmail.com>
2016-11-02 23:31:32 +0200

    cleaning

--- app/src/main/java/com/selfapps/rav/alltogether/firebaseDao/FirebaseHelper.java
+++ app/src/main/java/com/selfapps/rav/alltogether/firebaseDao/FirebaseHelper.java
@@ -13,7 +13,6 @@ import com.selfapps.rav.alltogether.model.Group;
 import com.selfapps.rav.alltogether.model.GroupReference;
 import com.selfapps.rav.alltogether.model.Member;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -22,14 +21,13 @@ import static com.selfapps.rav.alltogether.utilites.DbPath._authEmail;
 import static com.selfapps.rav.alltogether.utilites.DbPath._authUserId;
 import static com.selfapps.rav.alltogether.utilites.DbPath._groupReferences;
 import static com.selfapps.rav.alltogether.utilites.DbPath._lastUpdate;
-import static com.selfapps.rav.alltogether.utilites.DbPath._members;
 
 public class FirebaseHelper {
 
     private static final String TAG = "FirebaseHelper";
     private final DatabaseReference db;
     private final DatabaseReference dbRoot = FirebaseDatabase.getInstance().getReference();
-    List<GroupReference> groupReferences = new LinkedList<>();
+    private List<GroupReference> groupReferences = new LinkedList<>();
 
 
     public FirebaseHelper(DatabaseReference db) {
@@ -40,33 +38,20 @@ public class FirebaseHelper {
 
 
 
-    public String addGroupReference(GroupReference groupReference) {
+    private String addGroupReference(GroupReference groupReference) {
         DatabaseReference dbRef = db.child(_groupReferences);
         return setValue(dbRef,groupReference);
     }
 
 
     public String addGroup(Group group){
-        final Group g = group;
         group.addMember(new Member(_authUserId,_authEmail,"coordinator"));
         final DatabaseReference dbRef = dbRoot.child(_Groups);
         final String key = setValue(dbRef,group);
-        addGroupReference(new GroupReference(key,g.getName(),"coordinator"));
+        addGroupReference(new GroupReference(key,group.getName(),"coordinator"));
         return key;
     }
 
-
-
-    private void addMembers(ArrayList<Member> members, DatabaseReference dbRef) {
-        for (Member m : members ) {
-            addMember(m,dbRef);
-        }
-    }
-
-    private String addMember(Member m, DatabaseReference db) {
-        DatabaseReference dbRef = db.child(_members);
-        return setValue(dbRef,m);
-    }
 
     public List<GroupReference> retreive(){
         db.addChildEventListener(new ChildEventListener() {
