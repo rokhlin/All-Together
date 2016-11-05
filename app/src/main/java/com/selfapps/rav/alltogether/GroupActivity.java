551a6b354d48596895c24960e22716e6aa510c4f
rokhlin <rohlinav@gmail.com>
2016-11-05 21:56:53 +0200

    code refactoring  in GroupActivity
    create SelectionPagerAdapter

--- app/src/main/java/com/selfapps/rav/alltogether/GroupActivity.java
+++ app/src/main/java/com/selfapps/rav/alltogether/GroupActivity.java
@@ -19,26 +19,17 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.selfapps.rav.alltogether.Adapters.SectionsPagerAdapter;
 import com.selfapps.rav.alltogether.fragments.EventsFragment;
 import com.selfapps.rav.alltogether.fragments.GroupDetailFragment;
 import com.selfapps.rav.alltogether.fragments.MembersFragment;
+import com.selfapps.rav.alltogether.model.GroupReference;
 
 public class GroupActivity extends AppCompatActivity {
-    static String groupId;
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
+    public static String groupId;
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
+    public GroupReference groupReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,16 +38,14 @@ public class GroupActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         Intent intent = getIntent();
-       groupId = intent.getStringExtra("groupId");
+        groupId = intent.getStringExtra("groupId");
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,115 +60,18 @@ public class GroupActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_group, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-//    /**
-//     * A placeholder fragment containing a simple view.
-//     */
-//    public static class PlaceholderFragment extends Fragment {
-//        /**
-//         * The fragment argument representing the section number for this
-//         * fragment.
-//         */
-//        private static final String ARG_SECTION_NUMBER = "section_number";
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        /**
-//         * Returns a new instance of this fragment for the given section
-//         * number.
-//         */
-//        public static PlaceholderFragment newInstance(int sectionNumber) {
-//            PlaceholderFragment fragment = new PlaceholderFragment();
-//            Bundle args = new Bundle();
-//            args.putString("groupId", groupId);
-//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//            fragment.setArguments(args);
-//            return fragment;
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            View rootView = null;
-//
-//            switch (getArguments().getInt(ARG_SECTION_NUMBER)){
-//                case 1:
-//                    rootView = inflater.inflate(R.layout.fragment_group, container, false);
-//                    TextView tv_coordinator =(TextView) rootView.findViewById(R.id.tw_coordinator);
-//                    TextView tv_groupName =(TextView) rootView.findViewById(R.id.tw_group_name);
-//                    break;
-//                case 2:
-//                    rootView = inflater.inflate(R.layout.fragment_group_members, container, false);
-//                    break;
-//                case 3:
-//                    rootView = inflater.inflate(R.layout.fragment_group, container, false);
-//                    break;
-//            }
-//
-//
-//            return rootView;
-//        }
-//    }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    EventsFragment eventsFragment = new EventsFragment();
-                    return eventsFragment;
-                case 1:
-                    MembersFragment membersFragment = new MembersFragment();
-                    return membersFragment;
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Events";
-                case 1:
-                    return "Members";
-                default:
-                    return null;
-            }
-        }
-    }
 }
